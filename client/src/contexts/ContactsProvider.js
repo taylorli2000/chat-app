8d018367f5d15dac76fd0b9b34b35d536ef822d5
unknown <t4ylor.li@gmail.com>
2021-06-13 13:55:34 -0400

    finish chat-app

---
+++ client/src/contexts/ContactsProvider.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import UseLocalStorage from "../hooks/UseLocalStorage";
+
+const ContactsContext = React.createContext();
+
+export function useContacts() {
+  return useContext(ContactsContext);
+}
+
+export function ContactsProvider({ children }) {
+  const [contacts, setContacts] = UseLocalStorage("contacts", []);
+
+  function createContact(id, name) {
+    setContacts((prevContacts) => {
+      return [...prevContacts, { id, name }];
+    });
+  }
+
+  return (
+    <ContactsContext.Provider value={{ contacts, createContact }}>
+      {children}
+    </ContactsContext.Provider>
+  );
+}
